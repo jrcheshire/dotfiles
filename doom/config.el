4896b744ba2234d218047b8ded6b5d445b324616
Jamie Cheshire <cheshire@caltech.edu>
2025-04-17 13:23:30 -0700

    update doom configs

--- doom/config.el
+++ doom/config.el
@@ -32,7 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+;;(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-old-hope)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -59,6 +60,9 @@
 (add-hook 'lsp-before-initialize-hook 'conda-env-activation-hook)
 (defun conda-env-activation-hook ()
   (conda-env-activate (getenv "CONDA_DEFAULT_ENV")))
+
+;; who watches the watchers?
+(setq lsp-enable-file-watchers nil)
 
 (after! evil
   (setq evil-magic nil))
