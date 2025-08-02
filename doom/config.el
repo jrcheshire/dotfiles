256febf00f1ed188690a06c09b3d45a651919a2c
Jamie Cheshire <cheshire@umn.edu>
2025-08-02 02:27:06 -0700

    Bug fix...

--- doom/config.el
+++ doom/config.el
@@ -35,7 +35,7 @@
 ;;(setq doom-theme 'doom-one)
 ;;(setq doom-theme 'doom-old-hope)
 ;;(setq doom-theme 'doom-bluloco-dark)
-(setq doom-theme 'doom-city-lights) *
+(setq doom-theme 'doom-city-lights)
 ;;(setq doom-theme 'doom-moonlight)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
